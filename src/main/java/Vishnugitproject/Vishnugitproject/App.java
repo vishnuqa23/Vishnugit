e2838f60b497429ae9f9c4e60da179f5db98c214
VISHNU <vishnu.qa23@gmail.com>
2017-09-07 17:36:09 +0530

    myfirstgitproject

---
+++ src/main/java/Vishnugitproject/Vishnugitproject/App.java
@@ -0,0 +1,13 @@
+package Vishnugitproject.Vishnugitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
